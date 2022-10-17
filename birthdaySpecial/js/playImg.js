076d731946459a7b43ca06f673e4de87aa236bca
En8271 <115988620+En8271@users.noreply.github.com>
2022-10-17 15:47:30 +0530

    Add files via upload

---
+++ birthdaySpecial/js/playImg.js
@@ -0,0 +1,119 @@
+
+var btn = document.getElementById("heartTxt");
+btn.style.opacity = 0;
+var btnVal = 0;
+const audio = document.querySelector("audio");
+// const gcd = document.getElementById("a").value
+
+function showImage(){
+	document.getElementById("imgTxt").style.opacity = 0;
+	myImage.setAttribute("src", imageArray[imageIndex]);
+	myTxt.innerHTML = txtArray[imageIndex];
+	document.getElementById("imgTxt").style.opacity = 1 - flag;
+	imageIndex++;
+	if(imageIndex >= len){
+		imageIndex = 0;
+	}
+}
+function showImage1(){
+	document.getElementById("imgTxt").style.opacity = 0;
+	myImage.setAttribute("src", imageArray[imageIndex]);
+	myTxt.innerHTML = txtArray1[imageIndex];
+	document.getElementById("imgTxt").style.opacity = 1 - flag;
+	imageIndex++;
+	if(imageIndex >= len){
+		imageIndex = 0;
+	}
+}
+
+function durr(a){
+	if (a=="motu"){
+		document.getElementById('a').remove();
+		play();
+	}
+	else{
+		document.getElementById('a').remove();
+		play1();
+	}
+}
+
+function play(){
+	if(t == 0){
+		myImage.setAttribute("src", "");
+		myTxt.innerHTML = "";
+		imageIndex = 0;
+		clearInterval(showImageInterval);
+		audio.volume= 0.6;
+		audio.play();
+	}
+	flag = 1 - flag;
+	document.getElementById("typeDiv").style.opacity = flag;
+	document.getElementById("imgTxt").style.opacity = 1 - flag;
+	if(t == 0){
+		//setTimeout(showImage, 1000);
+		setInterval(showImage, 6000);
+	}
+	t++;
+}
+
+function play1(){
+	if(t == 0){
+		myImage.setAttribute("src", "");
+		myTxt.innerHTML = "";
+		imageIndex = 0;
+		clearInterval(showImageInterval);
+		audio.volume= 0.9;
+		audio.play();
+	}
+	flag = 1 - flag;
+	document.getElementById("typeDiv").style.opacity = flag;
+	document.getElementById("imgTxt").style.opacity = 1 - flag;
+	if(t == 0){
+		//setTimeout(showImage, 1000);
+		setInterval(showImage1, 6000);
+	}
+	t++;
+}
+
+function preshowImage(){
+	document.getElementById("imgTxt").style.opacity = 0;
+	myImage.setAttribute("src", imageArray[imageIndex]);
+	myTxt.innerHTML = txtArray[imageIndex];
+	imageIndex++;
+	if(imageIndex >= len){
+		imageIndex = 0;
+	}
+}
+
+function buttonFadeIn(){
+	if(btnVal < 1){
+		btnVal += 0.025;
+		btn.style.opacity = btnVal;
+	}
+	else{
+		clearInterval(buttonInterval);
+		if(ok == 3){
+			ok += 1;
+		}
+	}
+}
+
+
+
+function event(){
+
+	showImageInterval = setInterval(preshowImage, 100);
+
+	imgInterval = setInterval(function (){
+		if(ok == 3){
+			setTimeout(function(){buttonInterval = setInterval(buttonFadeIn, 50);}, 1500);
+			clearInterval(imgInterval);
+		}
+	}, 50);
+}
+
+var showImageInterval;
+var imgInterval;
+var buttonInterval;
+
+event();
